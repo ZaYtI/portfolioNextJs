332c82c4ff93d7d70d14454a8b6c366255b932de
Riquart Antonin <Antonin_riquart@ens.univ-artois.fr>
2023-12-31 14:10:41 +0100

    add footer address

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,23 @@
 import styles from './layout.module.css'
+import Link from 'next/link'
 export default function Footer() {
   return(
     <div className={styles.footer_wrapper}>
       <h1 className={styles.footer_title}>Antonin Riquart Portfolio</h1>
+      <ul>
+        <li>
+          Titulaire du permis B et A
+        </li>
+        <li>
+          <Link href="tel:+330760405456">07.60.40.64.56</Link>
+        </li>
+        <li>
+          <Link href="mailto:antonin.riquart@gmail.com">antonin.riquart@gmail.com</Link>
+        </li>
+        <li>
+          <Link href="https://maps.app.goo.gl/ryLbVuLFrMavTwcFA">adresse</Link>
+        </li>
+      </ul>
     </div>
   )
 }
