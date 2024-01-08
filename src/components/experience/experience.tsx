cef119def8b9aaefa7cccdd45582f8d548735ea1
Riquart Antonin <Antonin_riquart@ens.univ-artois.fr>
2024-01-09 00:11:48 +0100

    add professional experiences

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,35 +1,45 @@
 import Link from "next/link";
-import styles from '../../app/styles/experience.module.css'
+import styles from '../../app/styles/experience.module.css';
 
-export default function ExperienceComponent(){
-  return(
+type ExperienceBoxProps = {
+  title: string;
+  date: string;
+  href: string;
+};
+
+const ExperienceBox: React.FC<ExperienceBoxProps> = ({ title, date, href }) => (
+  <div className={styles.box}>
+    <h3 className={styles.title}>{title}</h3>
+    <p className={styles.date}>{date}</p>
+    <Link className={styles.btn} href={href}>
+      En savoir plus
+    </Link>
+  </div>
+);
+
+const experiences = [
+  { title: "Uneal", date: "juin 2023 - aout 2023", href: "https://uneal.com/" },
+  { title: "Uneal", date: "juin 2022 - aout 2022", href: "https://uneal.com/" },
+  {
+    title: "Gardien de déchèterie",
+    date: "juillet 2021 - aout 2021",
+    href: "https://www.bethunebruay.fr/fr/annuaire-des-sites-communautaires-et-mairies/dechetterie-de-bruay-la-buissiere",
+  },
+  {
+    title: "Conducteur de ligne Pasquier",
+    date: "mars 2021 - juillet 2021",
+    href: "https://www.pasquier.fr/",
+  },
+  { title: "Stages Aztek.lu", date: "avril 2023 - juin 2023", href: "https://new.aztek.lu/" },
+];
+
+const ExperienceComponent = () => (
   <div className={styles.container}>
     <h1 className={`${styles.title} ${styles.top_title}`}>Experiences Pro</h1>
-    <div className={styles.box}>
-      <h3 className={styles.title}>Uneal</h3>
-      <p className={styles.date}>juin 2023 - aout 2023</p>
-      <Link className={styles.btn} href="https://uneal.com/">En savoir plus</Link>
-    </div>
-    <div className={styles.box}>
-      <h3 className={styles.title}>Uneal</h3>
-      <p className={styles.date}>juin 2022 - aout 2022</p>
-      <Link className={styles.btn} href="https://uneal.com/">En savoir plus</Link>
-    </div>
-    <div className={styles.box}>
-      <h3 className={styles.title}>Gardien de déchèterie</h3>
-      <p className={styles.date}>juillet 2021 - aout 2021</p>
-      <Link className={styles.btn} href="https://www.bethunebruay.fr/fr/annuaire-des-sites-communautaires-et-mairies/dechetterie-de-bruay-la-buissiere">En savoir plus</Link>
-    </div>
-    <div className={styles.box}>
-      <h3 className={styles.title}>Conducteur de ligne Pasquier</h3>
-      <p className={styles.date}>mars 2021 - juillet 2021</p>
-      <Link className={styles.btn} href="https://www.pasquier.fr/">En savoir plus</Link>
-    </div>
-    <div className={styles.box}>
-      <h3 className={styles.title}>Stages Aztek.lu</h3>
-      <p className={styles.date}>avril 2023 - juin 2023</p>
-      <Link className={styles.btn} href="https://new.aztek.lu/">En savoir plus</Link>
-    </div>
+    {experiences.map((experience, index) => (
+      <ExperienceBox key={index} {...experience} />
+    ))}
   </div>
-  )
-}
+);
+
+export default ExperienceComponent;
